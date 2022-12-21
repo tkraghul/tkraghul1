94a760b1099098b080fe79ea7f082a8e2d6e7fc5
tkraghul <91047850+tkraghul@users.noreply.github.com>
2022-12-21 13:42:54 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+
+
+# In[2]:
+
+
+pi = math.pi
+
+
+# In[3]:
+
+
+r = float(12/2)
+
+
+# In[4]:
+
+
+V = (float(4/3)*(pi)*(r)**3)
+
+
+# In[5]:
+
+
+print(V)
+
+
+# In[ ]:
+
+
+
+
