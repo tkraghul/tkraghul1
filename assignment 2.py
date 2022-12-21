9985175bd89c245bbd25ae0e4e53febf35ca947e
tkraghul <91047850+tkraghul@users.noreply.github.com>
2022-12-21 13:36:57 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+firstname = input("What is your first name?:")
+lastname = input("What's your last name?: ")
+fn = firstname[::-1]
+ln = lastname[::-1]  
+print (fn,"",ln)
+
+
+# In[ ]:
+
+
+
+
